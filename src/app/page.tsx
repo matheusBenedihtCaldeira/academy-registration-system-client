b12a98cfbfad8de4991ab07564798d637f30df17
bene <matheusbenediht@gmail.com>
2023-08-16 17:36:39 -0300

    Separando compoenentes da tela

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,13 @@
 export default function Home() {
     return (
-        <div>
-            <div>
-                <aside>
-                    <nav>nav bar</nav>
+        <div className="h-screen flex flex-col">
+            <div className="flex flex-1 p-6">
+                <aside className="w-64">
+                    <nav className="space-y-6">nav bar</nav>
                 </aside>
-                <main>main</main>
+                <main className="flex-1 p-6">main</main>
             </div>
-            <footer>footer</footer>
+            <footer className="border-t p-6">footer</footer>
         </div>
     )
 
