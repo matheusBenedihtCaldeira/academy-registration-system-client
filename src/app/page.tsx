5dae82b1fa38bcd9ee414111287372fd4d143eab
bene <matheusbenediht@gmail.com>
2023-08-16 18:58:39 -0300

    Side bar

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,25 @@
+import {Home as HomeIcon, Search, Users} from 'lucide-react';
+
 export default function Home() {
     return (
         <div className="h-screen flex flex-col">
-            <div className="flex flex-1 p-6">
-                <aside className="w-64">
-                    <nav className="space-y-6">nav bar</nav>
+            <div className="flex flex-1">
+                <aside className="w-64 bg-slate-700 p-6">
+                    <nav className="space-y-6">
+                        <a href="" className="flex items-center gap-3 text-base font-semibold hover::bg-slate-500">
+                            <HomeIcon />
+                            Home
+                        </a>
+                        <a href="" className="flex items-center gap-3 text-base font-semibold hover::bg-slate-500">
+                            <Search />
+                            Buscar
+                        </a>
+                        <a href="" className="flex items-center gap-3 text-base font-semibold hover::bg-slate-500">
+                            <Users />
+                            Clientes
+                        </a>
+                        
+                    </nav>
                 </aside>
                 <main className="flex-1 p-6">main</main>
             </div>
